e4baa14af94cd8aced2a09010adb81ca0fb3bdc2
Emanuele Giacomini <giacomini@diag.uniroma1.it>
2024-10-09 13:09:06 +0200

    Fixed other compatibility issues

--- solutions/rp_01_compilers_and_build_systems/executables/many_object_in_stack_example.cpp
+++ solutions/rp_01_compilers_and_build_systems/executables/many_object_in_stack_example.cpp
@@ -1,49 +1,26 @@
-#include <iostream>
+#include <data_structures/matrix.h>
+#include <messages/odometry_message.h>
+#include <messages/pose_message.h>
 #include <utils/deserializer.h>
 #include <utils/identifiable.h>
+#include <utils/object_data.h>
 #include <utils/property_container_manager.h>
 #include <utils/serialization_context.h>
-#include <utils/object_data.h>
-#include <messages/pose_message.h>
-#include <messages/odometry_message.h>
-#include <data_structures/matrix.h>
+
+#include <iostream>
 
 using namespace mini_core;
 
 int main(int argc_, char** argv_) {
+  PoseMessage pose_msg;
+  OdometryMessage odom_msg;
+  Deserializer deserializer;
+  Identifiable identifiable;
+  BoolData data(true);
+  DynamicLoaderConfig dynloader;
+  SerializationContext ctx;
+  MatrixInt m;
 
-PoseMessage pose_msg;
-OdometryMessage odom_msg;
-Deserializer deserializer;
-Identifiable identifiable;
-BoolData data(true);
-DynamicLoaderConfig dynloader;
-SerializationContext ctx;
-MatrixInt m;
-
-
-
-std::cout << R"(
- _______________________________________________________
-/                                                       \
-|  Congratulations, you have completed this homework!   |
-\_________              _______________________________/ 
-          \_         __/    ___---------__               
-            \      _/      /              \_             
-             \    /       /                 \            
-              |  /       | _    _ \          \           
-              | |       / / \  / \ |          \          
-              | |       ||   ||   ||           |         
-              | |       | \_//\\_/ |           |         
-              | |       |_| (||)   |_______|   |         
-              | |         |  ||     | _  / /   |         
-               \ \        |_________|| \/ /   /          
-                \ \_       |_|_|_|_|/|  _/___/           
-                 \__>       _ _/_ _ /  |                 
-                          .|_|_|_|_|   |                 
-                          |           /                  
-                          |__________/                   
-
-)" << '\n';  
-    
+  std::cout << "Congratulations, you solved this homework!" << std::endl;
+  return 0;
 }
